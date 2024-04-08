f638eb80988b84aa969f724ef43c0d8adc84b40d
Rishabh Mehta <rishabhmehta@Masters-iMac-64.local>
2024-04-08 15:35:34 +0530

    added mock service worker setup

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
-app.use(createPinia())
-app.use(router)
+  const { worker } = await import("@/mocks/browser");
 
-app.mount('#app')
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+enableMocking().then(() => {
+    app.mount("#app")
+})
