8eb38122a715546d051af0ac4d7ae6124cb7319b
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2024-04-10 03:38:00 +0530

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    // override/add rules settings here, such as:
+    'vue/no-unused-vars': ['error'],
+    'vue/no-unused-refs': ['error'],
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-duplicate-imports': ['error'],
+    'no-unused-vars': ['error'],
+    'vue/no-export-in-script-setup': ['error']
+  },
+}
