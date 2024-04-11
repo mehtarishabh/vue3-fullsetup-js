b8baf12823536b0264a64d4f4f482dbb4edbc315
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2024-04-11 13:01:15 +0530

    dayjs and utils like localstorage, session storage etc

---
+++ src/utils/isMobile.ts
@@ -0,0 +1,7 @@
+export const getDeviceDetials = () => {
+  return {
+    isMobile: window.innerWidth < 768,
+    isTablet: window.innerWidth >= 768 && window.innerWidth < 1024,
+    isDesktop: window.innerWidth >= 1024,
+  };
+};
